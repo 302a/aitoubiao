d17e1231b8009420a152fc6d6aa2f9c08ba8923f
ushio <3096416399@qq.com>
2018-12-20 15:25:21 +0800

    创建models

--- aitoubiao_web/App/models.py
+++ aitoubiao_web/App/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
 # Create your models here.
+
+# 行业资讯表
+class industry_information(models.Model):
+    title = models.TextField()
+    date = models.CharField(max_length=256)
+    origin = models.CharField(max_length=256)
+    content = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'industry_information'
+
+
+class Announcement(models.Model):
+    title = models.TextField()
+    date = models.CharField(max_length=256)
+    view_number = models.CharField(max_length=16)
+    content = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'Announcement'
+
+class User(models.Model):
+    userid = models.CharField(max_length=16)
+    username = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
+    user_icon = models.ImageField(upload_to='icons')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
