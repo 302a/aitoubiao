55f90005aa19ff02e95ecadfabda87b89836b80e
辉 <823631152@qq.com>
2018-12-25 14:53:52 +0800

    保存web网站导航链接

--- aitoubiao_web/aitoubiao_web/urls.py
+++ aitoubiao_web/aitoubiao_web/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('App.urls')),
+    url(r'^api/', include('App.urls',namespace='web')),
 ]
