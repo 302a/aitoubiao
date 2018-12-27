da3e18f392e2405654463a06470e74923c58038e
zy <994124873@qq.com>
2018-12-27 10:58:22 +0800

    user_help 添加修改信息

--- aitoubiao_web/App/user_help.py
+++ aitoubiao_web/App/user_help.py
@@ -19,8 +19,8 @@ def help_login(username, password):
             # print(result[2])
             try:
                 # 执行sql语句
-                cursor_this.execute("insert into user(username, password, nickname) values ('{}', '{}', '{}')"
-                                    .format(result[1], result[2], result[3]))
+                cursor_this.execute("insert into user(username, password, nickname, user_icon) values ('{}', '{}', '{}', '{}')"
+                                    .format(result[1], result[2], result[3], result[4]))
                 # 提交到数据库执行
                 db_this.commit()
                 return '登录成功'
@@ -55,9 +55,9 @@ def help_register(username, nickname, password, user_icon):
         try:
             # 执行sql语句
             cursor_all.execute("insert into user(username, password, nickname, user_icon) values ('{}', '{}', '{}', '{}')"
-                                    .format(username, nickname, password, user_icon))
+                                    .format(username, password, nickname, user_icon))
             cursor_this.execute("insert into user(username, password, nickname, user_icon) values ('{}', '{}', '{}', '{}')"
-                                    .format(username, nickname, password, user_icon))
+                                    .format(username, password, nickname, user_icon))
             # 提交到数据库执行
             db_all.commit()
             db_this.commit()
@@ -70,8 +70,34 @@ def help_register(username, nickname, password, user_icon):
     db_all.close()
     db_this.close()
 
+def help_compile(username, password, nickname, user_icon):
+    db_all = pymysql.connect("192.168.101.14", "root", "shaoyang", "user")
+    db_this = pymysql.connect("192.168.101.14", "root", "shaoyang", "aitoubiao")
+
+    cursor_all = db_all.cursor()
+    cursor_this = db_this.cursor()
+
+    cursor_all.execute("select * from user where username = '{}'".format(username))
+
+    results = cursor_all.fetchall()
+
+    if len(results) == 1:
+        try:
+            cursor_all.execute("update user set password='{}', nickname='{}', user_icon='{}' where username='{}'"
+                               .format(password, nickname, user_icon, username))
+            db_all.commit()
+            return '修改成功'
+        except:
+            db_all.rollback()
+            return '未知错误'
+    else:
+        return '未知错误'
+
+
+
+
 
 
 if __name__ == '__main__':
-    print(help_login('a', '1'))
+    print(help_compile('a', '111', 'aaaa', 'icontest'))
 
